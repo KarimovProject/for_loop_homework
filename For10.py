dd4b627fd32a7bc29eacdeea1ded9bcc7b453928
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-04 09:11:20 -0400

    10-misol

--- For10.py
+++ For10.py
@@ -6,4 +6,8 @@ def main(list1):
     Returns:
         list: return  answer
     """
-    return
+    s = []
+    for i in list1:
+        s.append(i.title())
+    return s
+print(main(['javohir', 'murod', 'ogabek', 'umid']))
